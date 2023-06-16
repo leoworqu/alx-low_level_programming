b253908c49ef163735abe469035a252883009ed4
leoworqu <leoworqu@gmail.com>
2023-06-16 09:02:32 -0700

    2B

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -16,11 +16,11 @@
 int main(void)
 {
 	char alphabet = 'a';
-	while (alphabet <= 'z')
+	while(alphabet <= 'z')
 	{
 		putchar(alphabet);
-		putchar('\n');
 		alphabet++;
 	}
+	putchar('\n');
 	return (0);
 }
