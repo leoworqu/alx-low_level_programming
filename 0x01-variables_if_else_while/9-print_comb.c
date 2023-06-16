91c8c4ab1c6938ca6bb178de05c48394262a70fe
leoworqu <leoworqu@gmail.com>
2023-06-16 10:54:21 -0700

    9B

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,8 +11,11 @@ int main(void)
 	while (i <= 9)
 	{
 		putchar(i + '0');
-		putchar(',');
-		putchar(' ');	
+		while(i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 		i++;
 	}
 	putchar('\n');
