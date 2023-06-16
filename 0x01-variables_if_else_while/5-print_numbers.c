de680310bd2a0f5bf37b73dc1ab8a9d026633646
leoworqu <leoworqu@gmail.com>
2023-06-16 09:56:29 -0700

    5A

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base
+ * 10 starting from 0, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
