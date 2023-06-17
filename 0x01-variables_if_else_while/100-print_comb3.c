a5a324a88a0c9620429aed2c19c6f50ff8dff81e
leoworqu <leoworqu@gmail.com>
2023-06-16 22:17:49 -0700

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,5 +6,22 @@
  */
 int main(void)
 {
+	int i = 0;
+
+	while (i < 9)
+	{
+		int j = i + 1;
+
+		while (j < 9)
+		{
+			putchar(i , '0');
+			putchar(j , '0');
+			putchar(',');
+			putchar(' ');
+			j++;
+		}
+		i++;
+	}
+	puchar('\n');
 	return (0);
 }
