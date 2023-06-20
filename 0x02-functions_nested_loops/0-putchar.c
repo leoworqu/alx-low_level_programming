e7db188067db6871345102c90a2ced6f8ae67e1a
leoworqu <leoworqu@gmail.com>
2023-06-20 00:19:16 -0700

    0A

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include "unistd.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ *
+ *
+ */
+
+int _putchar(char c);
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
