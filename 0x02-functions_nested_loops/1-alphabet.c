4c9269b6c83f2825de9b50f9e28a06e9688af10a
leoworqu <leoworqu@gmail.com>
2023-06-20 10:38:16 -0700

    1K

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,13 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - print _putchar
  *
- * Return: Always 0.
+ *
+ * Return: 0
  */
+
 void print_alphabet(void)
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
 {
 	char i = 'a';
 	/**
