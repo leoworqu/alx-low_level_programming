74531df4037afba81a1a622d8978042e239d11ed
leoworqu <leoworqu@gmail.com>
2023-06-29 08:49:07 -0700

    0A

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * dest is the destnation src is string to be contacinated to dest. 
+ * ptr is the pointer to dest to count and store location.
+ *
+ * return dest`
+ *
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *ptr = dest;
+
+	while (*ptr != '\0')
+	{
+		ptr++;
+	}
+	while (*src !=  '\0')
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+	}
+	*ptr = '\0';
+	return (dest);
+}
