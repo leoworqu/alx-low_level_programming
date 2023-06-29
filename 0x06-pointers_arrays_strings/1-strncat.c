1b04332be64cf1a7a923252a622eb642f05af8e1
leoworqu <leoworqu@gmail.com>
2023-06-29 09:03:00 -0700

    1A

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/*
+ *
+ *
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *ptr = dest;
+
+	while (*ptr != '\0')
+	{
+		ptr++;
+	}
+
+	while (n > 0 && *src != '\0')
+	{
+		*ptr = *src;
+		*ptr++;
+		*src++;
+		n--;
+	}
+}
