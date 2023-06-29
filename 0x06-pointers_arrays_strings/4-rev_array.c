df27061fef2e41b1e00df794d6a4d4a11054186b
leoworqu <leoworqu@gmail.com>
2023-06-29 11:22:57 -0700

    4D

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,13 +8,13 @@
  */
 void reverse_array(int *a, int n)
 {
-	int *ptr = a + n - 1;
+	int  i;
 	int temp;
 
-	for (int i = 0; i < n / 2; i++)
+	for (i = 0; i < n--; i++)
 	{
-		temp = *(ptr - i);
-		*(ptr - i) = *(a + i);
-		*(a + i) = temp;
+		temp = a[i];
+		a[i] = a[n];
+		a[n] = temp;
 	}
 }
