7af62896f3d6e2f9077cec4a5613ec7b00b2456e
leoworqu <leoworqu@gmail.com>
2023-06-30 11:13:59 -0700

    abcd

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,23 +8,32 @@
  */
 char *cap_string(char *str)
 {
-	int i = 0;
+	int index = 0;
 
-	while (str[i])
+	while (str[index])
 	{
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
-		{
-			i++;
-		}
-		if (str[i] == ' ' || str[i] == '\t' ||
-			str[i] == '\n' || str[i] == ',' || str[i] == ';' ||
-			str[i] == '.' || str[i] == '!' ||
-			str[i] == '?' || str[i] == '"' || str[i] == '(' ||
-			str[i] == ')' || str[i] == '{' || str[i] == '}' || i = 0)
-		{
-			str[i] = str[i] - 32;
-		}
-		i++;
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
+			str[index] -= 32;
+
+		index++;
 	}
+
 	return (str);
+
 }
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * leet - encode into 1337speak
+ * @n: input value
+ * Return: n value
+ */
+char *leet(char *n)
+{
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (j = 0; n[j] != '\0'; j++)
+	{
+		for (i = 0; i < 10; i++)
+		{
+			if (n[j] == s1[i])
+			{
+				n[j] = s2[i];
+			}
+		}
+	}
+	return (n);
+}
+
