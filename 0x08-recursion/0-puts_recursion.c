b8679931d15bb7421dfd5c233ebcc551b773a540
leoworqu <leoworqu@gmail.com>
2023-07-05 03:43:28 -0700

    0a

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*
+ * @s a character
+ * _puts_recursion : recursive function to print strings line by line
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return (0);
+	}
+	_putchar(*s);
+	_puts_recursion(s - 1);
+}
