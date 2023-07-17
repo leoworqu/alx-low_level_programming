b07dca85fad091725b114fc6b6fcddc6aa98bd6a
leoworqu <leoworqu@gmail.com>
2023-07-17 04:34:03 -0700

    a

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the name of the file it was compiled from
+ *              followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
