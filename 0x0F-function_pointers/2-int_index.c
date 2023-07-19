0e98c0ce1402c3d5bc65ec26acdd65443a9b373a
leoworqu <leoworqu@gmail.com>
2023-07-19 15:35:10 -0700

    c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,12 +8,15 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int j;
 
-	for (i = 0; i < size; i++)
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+	for (j = 0; j < size; j++)
 	{
-		if (cmp(array[i]))
-			return (i);
+		if (cmp(array[j]))
+			return (j);
 	}
 	return (-1);
 }
