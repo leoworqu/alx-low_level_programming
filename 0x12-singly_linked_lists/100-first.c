6b8ad8d64a0a50a25fcd06f9d87aa2a685c35f5c
leoworqu <leoworqu@gmail.com>
2023-07-27 19:58:41 -0700

    v

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * before_main - Prints a specific message before the main function is executed
+ *
+ * Description: This function is executed automatically before the main function
+ *              is called. It prints the message "You're beat! and yet, you must allow,"
+ *              and "I bore my house upon my back!" to the standard output.
+ *
+ * Return: None
+ */
+
+void __attribute__ ((constructor)) before_main()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
