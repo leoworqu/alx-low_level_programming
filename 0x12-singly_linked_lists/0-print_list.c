72a9f09036dc18dec24a06245cb9fcfe3319f0fe
leoworqu <leoworqu@gmail.com>
2023-07-27 19:53:26 -0700

    Push

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints the contents of a linked list
+ * @h: pointer to the head of the linked list
+ *
+ * Return: the size of the linked list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
