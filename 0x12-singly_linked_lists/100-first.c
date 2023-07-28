a375531bb2ee522b8bfe76aae2ae34a5f5dacd89
leoworqu <leoworqu@gmail.com>
2023-07-27 20:00:39 -0700

    v

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
 /**
- * before_main - Prints a specific message before the main function is executed
+ * before_main - Prints a specific message before the main function is 
+ * executed
  *
- * Description: This function is executed automatically before the main function
- *              is called. It prints the message "You're beat! and yet, you must allow,"
- *              and "I bore my house upon my back!" to the standard output.
+ * Description: This function is executed automatically before 
+ * the main function
+ *              is called. It prints the message 
+ *              "You're beat! and yet, you must allow,"
+ *              and "I bore my house upon my back!" to the standard output.`:wq
+ *
  *
  * Return: None
  */
