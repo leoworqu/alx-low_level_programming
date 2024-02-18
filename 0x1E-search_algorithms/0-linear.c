566aacddb7f3096cbdaedc1f3b70219fddc98549
leoworqu <leoworqu@gmail.com>
2024-02-17 20:57:14 -0800

    linear

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,29 @@
+#include "search_algos.h" 
+
+/**
+ * linear_search - Search for a value in an array using linear search algorithm
+ * @array: Pointer to the first element of the array to search in
+ * @size: Number of elements in the array
+ * @value: Value to search for
+ *
+ * Return: The first index where value is located,
+ * or -1 if not found or array is NULL
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", (unsigned long)i, array[i]);
+		if (array[i] == value)
+		{
+			printf("Found %d at index: %lu\n", value, (unsigned long)i);
+			return (i);
+		}
+	}
+
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,1 @@
+this is he read me
